6d88991eaa570333801496256bdb2092b91f6fbc
Sibel Gündoğar <gundogarsibel@gmail.com>
2024-05-07 15:53:16 +0300

    commit

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, Image, FlatList, TouchableOpacity, ScrollView } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import cities from '../data/cities';
 
 import Messages from './Messages';
 import New from './New';
@@ -77,52 +79,39 @@ function Navbar() {
 
 //ana sayfanın kodları
 function HomeScreen({ navigation }) {
-  // Ana sayfanın içeriğinde görünmesi için bir kaç örnek ürün ilanı
-  const products = [
-    {
-      id: 1,
-      name: 'Cüzdan',
-      location: 'Altıeylül, Balıkesir',
-      image: require('../images/cuzdan.jpg'),
-      desc: 'Siyah deri bir cüzdan. İçinde kimilk veya kredi kartı yok, NEF civarında bulunmuştur.',
-    },
-    {
-      id: 2,
-      name: 'Kolye',
-      location: 'Karesi, Balıkesir',
-      image: require('../images/kolye.jpg'),
-      desc: 'Altın rengi kolye. Karesi Belediye Binasında bulunmuştur. Sahibi mesaj atarak ulaşabilir.',
-    },
-    {
-      id: 3,
-      name: 'Saat',
-      location: 'Eyüp, İstanbul',
-      image: require('../images/saat.jpg'),
-      desc: 'Saat sahibi saatiyle olan bi fotoğrafı göstererek teslim alabilir. Mesaj atın.',
-    },
-    {
-      id: 4,
-      name: 'Plaka',
-      location: 'Esenler, İstanbul',
-      image: require('../images/plaka.jpg'),
-      desc: ' nolu plakayı Esenler meydanda araç sahibi düşürmüş. Mesaj atarak ulaşabilirsiniz. ',
-    },
-    {
-      id: 5,
-      name: 'Kulaklık',
-      location: 'Urla, İzmir',
-      image: require('../images/kulaklik.jpg'),
-      desc: 'Urla da bir kafede bulundu. Kafeden teslim alabilirsiniz, sahibinin telefonuna otomatik bağlanırsa sahibine teslim edilecektir.',
-    },
-  ];
+  const [items, setItems] = useState([]);
+  const db = getFirestore();
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, 'items'));
+        const itemsData = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          itemsData.push({
+            id: doc.id,
+            title: data.title,
+            description: data.description,
+            city: data.city,
+            district: data.district,
+            photos: data.photos
+          });
+        });
+        setItems(itemsData);
+      } catch (error) {
+        console.error('Verileri alırken bir hata oluştu:', error);
+      }
+    };
+    fetchItems();
+  }, []);
 
   // Her bir ürünü render ediyor
   const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.productItemContainer} onPress={() => navigation.navigate('Ürün Detayı', { product: item })}>
-      <View style={styles.productItem}>
-        <Image source={item.image} style={styles.productImage} resizeMode="cover" />
-        <Text style={styles.productName}>{item.name}</Text>
-        <Text style={styles.productLocation}>{item.location}</Text>
+    <TouchableOpacity style={styles.itemItemContainer} onPress={() => navigation.navigate('Ürün Detayı', { items: item })}>
+      <View style={styles.itemItem}>
+        <Image source={{ uri: item.photos[0] }} style={styles.itemImage} resizeMode="cover" />
+        <Text style={styles.itemName}>{item.title}</Text>
+        <Text style={styles.itemLocation}>{item.city}, {item.district}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -131,20 +120,34 @@ function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <SearchBar style={styles.searchbar} placeholder="Bul..." lightTheme />
-        <FlatList data={products} renderItem={renderItem} keyExtractor={(item) => item.id.toString()} numColumns={2} />
+      <FlatList data={items} renderItem={renderItem} keyExtractor={(item) => item.id.toString()} numColumns={2} />
     </View>
   );
 }
 
 // Ürün detayı sayfası açılca geleek ekranın tasarımını içeren fonksiyon
-function ProductDetailScreen({ route }) {
-  const { product } = route.params;
+function ItemDetailScreen({ route }) {
+  const { items } = route.params;
+  const photos = items && items.photos ? items.photos : [];
+
+  // Tüm resimleri göstermek için renderItem fonksiyonu güncelleniyor
+  const renderItem = ({ item }) => (
+    <Image source={{ uri: item }} style={styles.itemDetailImage} resizeMode="cover" />
+  );
+  
+
   return (
     <View style={styles.container}>
-      <Image source={product.image} style={styles.productDetailImage} resizeMode="cover" />
-      <Text style={styles.productDetailName}>{product.name}</Text>
-      <Text style={styles.productDetailLocation}>{product.location}</Text>
-      <Text style={styles.productDetailDesc}>{product.desc}</Text>
+      <FlatList
+        horizontal
+        data={photos}
+        keyExtractor={(photo, index) => index.toString()}
+        renderItem={renderItem}
+      />
+      {/* <Image source={{ uri: items.photos[0] }} style={styles.itemImage} resizeMode="cover" /> */}
+      <Text style={styles.itemDetailName}>{items.title}</Text>
+      <Text style={styles.itemDetailLocation}>{items.city}, {items.district}</Text>
+      <Text style={styles.itemDetailDesc}>{items.description}</Text>
       <TouchableOpacity style={styles.sendButton}>
         <Text style={styles.sendButtonText}>Mesaj Gönder</Text>
       </TouchableOpacity>
@@ -152,12 +155,13 @@ function ProductDetailScreen({ route }) {
   );
 }
 
+
 //Ana sayfa ve Ürün detay sayfası arasında stack screen oluşturan fonksiyon
 function HomeWrapper() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Ürün Detayı" component={ProductDetailScreen} />
+      <Stack.Screen name="Ürün Detayı" component={ItemDetailScreen} />
     </Stack.Navigator>
   )
 }
@@ -175,11 +179,11 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: '#bdc6cf',
   },
-  productItemContainer: {
+  itemItemContainer: {
     flex: 1,
     margin: 5,
   },
-  productItem: {
+  itemItem: {
     flex: 1,
     alignItems: 'center',
     width: 170,
@@ -188,41 +192,41 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     padding: 10,
   },
-  productImage: {
+  itemImage: {
     width: '95%',
     height: 120,
     borderRadius: 5,
   },
-  productName: {
+  itemName: {
     fontSize: 16,
     fontWeight: 'bold',
     marginTop: 10,
   },
-  productLocation: {
+  itemLocation: {
     fontSize: 14,
     color: '#6700A9',
     marginTop: 5,
   },
-  productDetailImage: {
+  itemDetailImage: {
     alignSelf: 'center',
     width: 350,
     height: 250,
     borderRadius: 5,
     marginBottom: 15,
   },
-  productDetailName: {
+  itemDetailName: {
     marginLeft: 20,
     fontSize: 20,
     fontWeight: 'bold',
     marginTop: 10,
   },
-  productDetailLocation: {
+  itemDetailLocation: {
     marginLeft: 20,
     fontSize: 15,
     color: '#6700A9',
     marginTop: 5,
   },
-  productDetailDesc: {
+  itemDetailDesc: {
     marginLeft: 20,
     fontSize: 15,
     marginTop: 5,
--- screens/New.js
+++ screens/New.js
@@ -12,194 +12,194 @@ import firestore from 'firebase/firestore';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { useNavigation } from '@react-navigation/native';
 
-export default function New({navigation}) {
-	const [selectedPhotos, setSelectedPhotos] = useState(['addButton']);
-	const [modalVisible, setModalVisible] = useState(false);
-	const [title, setTitle] = useState('');
-	const [desc, setDesc] = useState('');
-	const [selectedCityIndex, setSelectedCityIndex] = useState(0);
-	const [selectedDistrictIndex, setSelectedDistrictIndex] = useState(0);
-	const db = getFirestore();
-	const storage = getStorage();
+export default function New({ navigation }) {
+  const [selectedPhotos, setSelectedPhotos] = useState(['addButton']);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [title, setTitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [selectedCityIndex, setSelectedCityIndex] = useState(0);
+  const [selectedDistrictIndex, setSelectedDistrictIndex] = useState(0);
+  const db = getFirestore();
+  const storage = getStorage();
 
-	const addPicture = () => {
-		setModalVisible(true);
-	}
+  const addPicture = () => {
+    setModalVisible(true);
+  }
 
-	const takePicture = async () => {
-		if (selectedPhotos.length >= 6) return;
-		const { status } = await Camera.requestCameraPermissionsAsync();
+  const takePicture = async () => {
+    if (selectedPhotos.length >= 6) return;
+    const { status } = await Camera.requestCameraPermissionsAsync();
 
-		if (status !== 'granted') {
-			console.log('Kamera izni reddedildi.');
-			return;
-		}
+    if (status !== 'granted') {
+      console.log('Kamera izni reddedildi.');
+      return;
+    }
 
-		const result = await launchCameraAsync({
-			aspect: [1, 1],
-			allowsEditing: true,
-			cameraType: CameraType.back,
-			mediaTypes: MediaTypeOptions.Images,
-			quality: 0.5,
-		})
+    const result = await launchCameraAsync({
+      aspect: [1, 1],
+      allowsEditing: true,
+      cameraType: CameraType.back,
+      mediaTypes: MediaTypeOptions.Images,
+      quality: 0.5,
+    })
 
-		if (result.canceled) return;
+    if (result.canceled) return;
 
-		let newPhotos = [...selectedPhotos];
-		newPhotos.splice(1, 0, result.assets[0].uri);
-		_setSelectedPhotos(newPhotos);
-	};
+    let newPhotos = [...selectedPhotos];
+    newPhotos.splice(1, 0, result.assets[0].uri);
+    _setSelectedPhotos(newPhotos);
+  };
 
-	const selectFromGallery = async () => {
-		if (selectedPhotos.length >= 6) return;
-		const result = await launchImageLibraryAsync({
-			aspect: [1, 1],
-			mediaTypes: MediaTypeOptions.Images,
-			quality: 0.5,
-			allowsMultipleSelection: true,
-			selectionLimit: 6 - selectedPhotos.length
-		})
+  const selectFromGallery = async () => {
+    if (selectedPhotos.length >= 6) return;
+    const result = await launchImageLibraryAsync({
+      aspect: [1, 1],
+      mediaTypes: MediaTypeOptions.Images,
+      quality: 0.5,
+      allowsMultipleSelection: true,
+      selectionLimit: 6 - selectedPhotos.length
+    })
 
-		if (result.canceled) return;
+    if (result.canceled) return;
 
-		let photos = result.assets.map((val) => val.uri)
+    let photos = result.assets.map((val) => val.uri)
 
-		let newPhotos = [...selectedPhotos];
-		newPhotos.splice(1, 0, ...photos);
-		_setSelectedPhotos(newPhotos);
-	}
+    let newPhotos = [...selectedPhotos];
+    newPhotos.splice(1, 0, ...photos);
+    _setSelectedPhotos(newPhotos);
+  }
 
-	const _setSelectedPhotos = (photos) => {
-		setSelectedPhotos(photos);
-		if (modalVisible && photos.length >= 6) setModalVisible(false);
-	}
+  const _setSelectedPhotos = (photos) => {
+    setSelectedPhotos(photos);
+    if (modalVisible && photos.length >= 6) setModalVisible(false);
+  }
 
-	const removePicture = (index) => {
-		let newPhotos = [...selectedPhotos];
-		newPhotos.splice(index, 1);
-		_setSelectedPhotos(newPhotos);
-	}
+  const removePicture = (index) => {
+    let newPhotos = [...selectedPhotos];
+    newPhotos.splice(index, 1);
+    _setSelectedPhotos(newPhotos);
+  }
 
-	const handleCityPicker = (value, index) => {
-		setSelectedCityIndex(index);
-	};
+  const handleCityPicker = (value, index) => {
+    setSelectedCityIndex(index);
+  };
 
-	const handleDistrictPicker = (value, index) => {
-		setSelectedDistrictIndex(index);
-	}
+  const handleDistrictPicker = (value, index) => {
+    setSelectedDistrictIndex(index);
+  }
 
-	const handleUpload = async () => {
+  const handleUpload = async () => {
     if (selectedPhotos.length <= 1 || !title || !desc || selectedCityIndex === 0) {
       Alert.alert('Lütfen tüm alanları doldurun.');
       return;
     }
-		try {
-			// Resimleri Firebase Storage'a yükle
-			const photoURLs = [];
-			for (const photo of selectedPhotos) {
-				if (photo !== 'addButton') {
-					const fileName = photo.split('/').pop();
-					const fileRef = ref(storage, fileName);
-					const response = await fetch(photo);
-					const blob = await response.blob();
-					// console.log("uploading")
-					await uploadBytes(fileRef, blob);
-					// console.log("uploaded");
-					const downloadURL = await getDownloadURL(fileRef);
-					photoURLs.push(downloadURL);
-				}
-			}
+    try {
+      // Resimleri Firebase Storage'a yükle
+      const photoURLs = [];
+      for (const photo of selectedPhotos) {
+        if (photo !== 'addButton') {
+          const fileName = photo.split('/').pop();
+          const fileRef = ref(storage, fileName);
+          const response = await fetch(photo);
+          const blob = await response.blob();
+          // console.log("uploading")
+          await uploadBytes(fileRef, blob);
+          // console.log("uploaded");
+          const downloadURL = await getDownloadURL(fileRef);
+          photoURLs.push(downloadURL);
+        }
+      }
 
 
-			const productData = {
-				title: title,
-				description: desc,
-				city: cities[selectedCityIndex].il_adi,
-				district: cities[selectedCityIndex].ilceler[selectedDistrictIndex].ilce_adi,
-				photos: photoURLs
+      const productData = {
+        title: title,
+        description: desc,
+        city: cities[selectedCityIndex].il_adi,
+        district: cities[selectedCityIndex].ilceler[selectedDistrictIndex].ilce_adi,
+        photos: photoURLs
 
-			};
+      };
 
-			const docRef = await addDoc(collection(db, "items"), productData);
-			console.log("Ürün başarıyla eklendi:", docRef.id);
+      const docRef = await addDoc(collection(db, "items"), productData);
+      console.log("Ürün başarıyla eklendi:", docRef.id);
       Alert.alert('Tebrikler', 'Ürün başarıyla eklendi.');
-     //sayfayı yeniliyor formun boşaltılması için
-			navigation.replace("Home", {screen: "New"})
-		} catch (error) {
-			console.error('Ürün yükleme hatası:', error);
+      //sayfayı yeniliyor formun boşaltılması için
+      navigation.replace("Home", { screen: "New" })
+    } catch (error) {
+      console.error('Ürün yükleme hatası:', error);
       Alert.alert('Hata', 'Ürün yüklenirken bir hatayla karşılaşıldı.');
-		}
-	};
+    }
+  };
 
-	return (
-		<ScrollView style={styles.container}>
-			<SelectionDrawer visible={modalVisible} onClose={() => { setModalVisible(false) }} options={[{ text: 'Camera', action: takePicture }, { text: 'Gallery', action: selectFromGallery }]} />
+  return (
+    <ScrollView style={styles.container}>
+      <SelectionDrawer visible={modalVisible} onClose={() => { setModalVisible(false) }} options={[{ text: 'Camera', action: takePicture }, { text: 'Gallery', action: selectFromGallery }]} />
 
-			<View style={styles.photoContainer}>
-				<FlatList horizontal data={selectedPhotos} renderItem={({ item, index }) => (
-					(item === 'addButton') ? (selectedPhotos.length >= 6) ? null : <SelectPhotoButton onPress={addPicture} /> : <SelectedPhoto onDelete={() => { removePicture(index) }} src={item} />
-				)}
-				/>
-			</View>
+      <View style={styles.photoContainer}>
+        <FlatList horizontal data={selectedPhotos} renderItem={({ item, index }) => (
+          (item === 'addButton') ? (selectedPhotos.length >= 6) ? null : <SelectPhotoButton onPress={addPicture} /> : <SelectedPhoto onDelete={() => { removePicture(index) }} src={item} />
+        )}
+        />
+      </View>
 
-			<Text style={styles.inputText}>Ürün Başlığı</Text>
-			<TextInput style={styles.input} placeholder="Ürün Başlığı" value={title} onChangeText={(text) => setTitle(text)} />
+      <Text style={styles.inputText}>Ürün Başlığı</Text>
+      <TextInput style={styles.input} placeholder="Ürün Başlığı" value={title} onChangeText={(text) => setTitle(text)} />
 
-			<Text style={styles.inputText}>Ürün Açıklaması</Text>
-			<TextInput style={styles.textArea} placeholder="Ürün Açıklaması" multiline value={desc} onChangeText={(text) => setDesc(text)} />
+      <Text style={styles.inputText}>Ürün Açıklaması</Text>
+      <TextInput style={styles.textArea} placeholder="Ürün Açıklaması" multiline value={desc} onChangeText={(text) => setDesc(text)} />
 
-			<InlineListPicker label='İl' items={cities} onSelect={handleCityPicker} renderItemLabel={(value, index) => value.il_adi} />
-			<InlineListPicker label='İlçe' items={cities[selectedCityIndex].ilceler} onSelect={handleDistrictPicker} renderItemLabel={(value, index) => {print(value); return value.ilce_adi;}} />
+      <InlineListPicker label='İl' items={cities} onSelect={handleCityPicker} renderItemLabel={(value, index) => value.il_adi} />
+      <InlineListPicker label='İlçe' items={cities[selectedCityIndex].ilceler} onSelect={handleDistrictPicker} renderItemLabel={(value, index) => { print(value); return value.ilce_adi; }} />
 
-			<TouchableOpacity style={styles.uploadButton}>
-				<Text style={styles.uploadButtonText} onPress={handleUpload}>Yükle</Text>
-			</TouchableOpacity>
-		</ScrollView>
-	);
+      <TouchableOpacity style={styles.uploadButton}>
+        <Text style={styles.uploadButtonText} onPress={handleUpload}>Yükle</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		padding: 20,
-		marginHorizontal: 10
-	},
-	photoContainer: {
-		borderWidth: 2,
-		borderColor: '#B97AFF',
-		borderStyle: 'dashed',
-		marginVertical: 20,
-		borderRadius: 10,
-		height: 125
-	},
-	inputText: {
-		fontSize: 15,
-		marginVertical: 10,
-	},
-	input: {
-		height: 40,
-		borderColor: 'gray',
-		borderWidth: 1,
-		padding: 10,
-		borderRadius: 10,
-		marginBottom: 20,
-	},
-	textArea: {
-		height: 80,
-		borderColor: 'gray',
-		borderWidth: 1,
-		marginBottom: 10,
-		padding: 10,
-		borderRadius: 10,
-		verticalAlign: 'top',
-	},
-	uploadButton: {
-		marginVertical: 20,
-		backgroundColor: '#B97AFF',
-		padding: 10,
-		borderRadius: 5,
-	},
-	uploadButtonText: {
-		color: 'white',
-		textAlign: 'center',
-	}
+  container: {
+    padding: 20,
+    marginHorizontal: 10
+  },
+  photoContainer: {
+    borderWidth: 2,
+    borderColor: '#B97AFF',
+    borderStyle: 'dashed',
+    marginVertical: 20,
+    borderRadius: 10,
+    height: 125
+  },
+  inputText: {
+    fontSize: 15,
+    marginVertical: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  textArea: {
+    height: 80,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10,
+    verticalAlign: 'top',
+  },
+  uploadButton: {
+    marginVertical: 20,
+    backgroundColor: '#B97AFF',
+    padding: 10,
+    borderRadius: 5,
+  },
+  uploadButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  }
 });
